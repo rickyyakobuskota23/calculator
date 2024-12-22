2775af0e7dd724021b22c4b81eea4a15e2d19830
rickyyakobuskota23 <rickykota13@gmail.com>
2024-12-22 12:13:06 +1100

    menambahkan javascript

---
+++ main.js
@@ -0,0 +1,33 @@
+const buttons = document.querySelectorAll('.mybutton');
+const display = document.getElementById('display-below');
+const deleteButton = document.getElementById('delete-button')
+const clearButton = document.getElementById('clear-button');
+const equalsButton = document.getElementById('equals');
+const displayAbove = document.getElementById('display-above')
+
+let result = 0;
+
+buttons.forEach(mybutton => {
+    mybutton.addEventListener('click', function() {
+        display.value += this.value;
+    });
+});
+
+deleteButton.addEventListener('click', function() {
+    display.value = display.value.slice(0, -1);
+});
+
+clearButton.addEventListener('click', function() {
+    display.value = '';
+    displayAbove.value = '';
+});
+
+equalsButton.addEventListener('click', function() {
+    try {
+        displayAbove.value = display.value;
+
+        display.value = eval(display.value);
+    } catch (error) {
+        display.value = 'error';
+    }
+});
